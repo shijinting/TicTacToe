ef1c4ceadb26974b9cbeaa8c14963ad2d96a68b2
shijinting <shijinting96@gmail.com>
2016-10-20 23:39:54 +0800

    Successfully implement 3x3, 4x4, 5x5 unbeatable tic tac toe

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class PlayGame {
 	public static void main(String[] args) {
 		String[] options = new String[] { "Cancel", "3x3", "4x4", "5x5" };
-		int mode = JOptionPane.showOptionDialog(null, "Please choose an option", "Little Hunter Chat",
+		int mode = JOptionPane.showOptionDialog(null, "Please choose an option", "Jin Tic Tac Toe",
 		JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 		
         JFrame window = new JFrame("Tic-Tac-Toe");
